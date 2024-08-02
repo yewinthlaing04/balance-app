99aa15417585842542ba198d35c2adc8b649db3f
YEWintHlaing <161342257+yewinthlaing04@users.noreply.github.com>
2024-08-02 12:14:31 +0200

    Delete Balance-Project directory

--- Balance-Project/src/main/java/com/ye/balance/balanceproject/model/repo/UserAccessLogRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ye.balance.balanceproject.model.repo;
-
-import com.ye.balance.balanceproject.model.domain.entity.UserAccessLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface UserAccessLogRepo extends JpaRepository<UserAccessLog , Integer> , JpaSpecificationExecutor<UserAccessLog > {
-
-}
